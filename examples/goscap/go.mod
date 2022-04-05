83fa8650bc88f09c34e62bd8a9578d4805f3b743
Frederico Araujo <frederico.araujo@ibm.com>
2022-04-04 21:57:03 -0400

    fix(examples/goscap): fix falcolibs package import path
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- examples/goscap/go.mod
+++ examples/goscap/go.mod
@@ -2,6 +2,6 @@ module goscap
 
 go 1.17
 
-replace falcolibs => /src/go/
+require github.com/sysflow-telemetry/libs-sdk/falcolibs v0.0.0-00010101000000-000000000000 // indirect
 
-require falcolibs v0.0.0-00010101000000-000000000000 // indirect
+replace github.com/sysflow-telemetry/libs-sdk/falcolibs => /src/go/
--- examples/goscap/goscap.go
+++ examples/goscap/goscap.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-        "falcolibs"
-	"os"
-	"fmt"
 	"C"
-       )
+	"fmt"
+	"os"
+
+	"github.com/sysflow-telemetry/libs-sdk/falcolibs"
+)
 
 func main() {
 	scapFile := os.Args[1]
@@ -15,16 +16,15 @@ func main() {
 	inspector.Open(scapFile)
 	for true {
 		res := inspector.Next(&ev)
-		if(res == falcolibs.SCAP_TIMEOUT) {
+		if res == falcolibs.SCAP_TIMEOUT {
 			fmt.Println("SCAP TIMEOUT")
-		} else if (res == falcolibs.SCAP_EOF){
+		} else if res == falcolibs.SCAP_EOF {
 			fmt.Println("SCAP EOF")
 			break
-		} else if (res != falcolibs.SCAP_SUCCESS){
+		} else if res != falcolibs.SCAP_SUCCESS {
 			fmt.Println("SCAP FAILURE")
 			break
 		}
-		//fmt.Println("SCAP SUCCESS")
 		fmt.Printf("%d %d %s\n", ev.GetRecordNum(), ev.GetTS(), ev.GetNameAsGoString())
 	}
 	inspector.Close()
