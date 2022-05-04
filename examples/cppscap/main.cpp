7152d85c25e19cbe31a65e9efaabbe6a30193e0a
Frederico Araujo <frederico.araujo@ibm.com>
2022-05-04 13:31:04 -0400

    chore(copyright): add copyright headers
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- examples/cppscap/main.cpp
+++ examples/cppscap/main.cpp
@@ -1,3 +1,18 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #include <sinsp.h>
 #include <cstdio>
 #include <csignal>
@@ -41,7 +56,7 @@ int main(int argc, char **argv) {
   }
 
   inspector->set_snaplen(0);
-  
+
   sinsp_evt *evt = 0;
   s_running = true;
   while (s_running) {
--- examples/cscap/main.c
+++ examples/cscap/main.c
@@ -1,3 +1,18 @@
+/*
+Copyright (C) 2022 The Falco Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #include "libs.h"
 #include <signal.h>
 #include <unistd.h>
@@ -18,9 +33,9 @@ int main(int argc, char **argv) {
     printf("usage: ./cscap [<scap file>]\n");
     exit(1);
   }
-  
+
   printf("Starting cscap\n");
-  
+
   CScapInspector inspector = ScapInspNew();
   ScapInspHostAndPortResolve(inspector, 0);
   if (argc == 2) {
@@ -36,9 +51,9 @@ int main(int argc, char **argv) {
      }
      printf("Started live capture...\n");
   }
-  
+
   ScapSetSnapLen(inspector, 0);
-  
+
   CScapEvent evt = 0;
   s_running = 1;
   while (s_running) {
